4731fab4e01cab9e8c3940e7bbfa629503a132e8
lilinjun1884 <lilinjunn@163.com>
2017-02-21 12:51:12 +0800

    xxx
    xxx

--- start-vpn.sh
+++ start-vpn.sh
@@ -5,7 +5,7 @@ apt-get install linux-image-extra-3.13.0-24-generic
 #查看系统现有内核
 dpkg -l|grep linux-image 
 #卸载列出的其他内核
-apt-get purge linux-image-3.13.0-104-generic linux-image-extra-3.13.0-104-generic 
+apt-get purge linux-image-3.13.0-105-generic linux-image-extra-3.13.0-105-generic 
 #更新 grub 系统引导文件并重启
 update-grub
 reboot
@@ -15,7 +15,7 @@ yum install python-setuptools && easy_install pip
 pip install shadowsocks
 
 # 启动shadowsocks(可以添加到/etc/rc.local) 
-ssserver -p 1984 -k zx123456 -m rc4-md5 -d start
+ssserver -p 1984 -k password -m rc4-md5 -d start
 
 #安装和运行锐速
 wget -N --no-check-certificate --restrict-file-names=nocontrol https://raw.githubusercontent.com/91yun/serverspeeder/master/serverspeeder-all.sh && bash serverspeeder-all.sh

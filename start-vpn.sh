89acb186d78476132f2acdd4f9b916906350ebba
lilinjun1884 <lilinjunn@163.com>
2017-02-22 23:22:07 +0800

    bbr
    --

--- start-vpn.sh
+++ start-vpn.sh
@@ -1,5 +1,13 @@
 #操作于：Ubuntu 14.04 x64
 
+#安装shadowsocks
+apt-get update
+apt-get install python-pip
+pip install shadowsocks
+# 启动shadowsocks(可以添加到/etc/rc.local) 
+ssserver -p 1984 -k yourpassword -m rc4-md5 -d start
+
+#选择一 使用锐速
 #内核换成24
 apt-get install linux-image-extra-3.13.0-24-generic
 #查看系统现有内核
@@ -9,13 +17,22 @@ apt-get purge linux-image-3.13.0-105-generic linux-image-extra-3.13.0-105-generi
 #更新 grub 系统引导文件并重启
 update-grub
 reboot
-
-#安装shadowsocks
-yum install python-setuptools && easy_install pip
-pip install shadowsocks
-
-# 启动shadowsocks(可以添加到/etc/rc.local) 
-ssserver -p 1984 -k password -m rc4-md5 -d start
-
 #安装和运行锐速
 wget -N --no-check-certificate --restrict-file-names=nocontrol https://raw.githubusercontent.com/91yun/serverspeeder/master/serverspeeder-all.sh && bash serverspeeder-all.sh
+
+#选择二 使用BBR
+#内核换成4.9
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9/linux-image-4.9.0-040900-generic_4.9.0-040900.201612111631_amd64.deb
+dpkg -i linux-image-4.9.0*.deb
+dpkg -l|grep linux-image
+apt-get purge #上面列出的非4.9的内核
+update-grub
+reboot
+#添加bbr配置
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl –p
+sysctl net.ipv4.tcp_available_congestion_control
+#查看BBR是否开启
+lsmod | grep bbr
+
